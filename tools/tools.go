4d14f0a770f80131302001e3c7e1474f1dcd66fe
kevinpollet <pollet.kevin@gmail.com>
2022-04-22 22:44:35 +0200

    Add tools package

--- tools/tools.go
+++ tools/tools.go
@@ -3,7 +3,7 @@
 
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies.
 // See: https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
-package hack
+package tools
 
 import (
 	_ "k8s.io/code-generator"
